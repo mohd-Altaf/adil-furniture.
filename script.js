f2179655ad0b6a4ab0bb01b5025df30814da0feb
Mohd Altaf <mohdaltaf495495@gmail.com>
2025-08-28 00:56:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-  const toggle = document.querySelector(".menu-toggle");
-  const menu = document.querySelector("nav ul");
+const toggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector("nav ul");
 
-  toggle.addEventListener("click", () => {
-    menu.classList.toggle("show");
-  });
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("show");
+      toggle.textContent = menu.classList.contains("show") ? "✖" : "☰";
+    });
