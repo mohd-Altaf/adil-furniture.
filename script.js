d78962465fd1bd3fbb6ca97d9be301c0770df650
Mohd Altaf <mohdaltaf495495@gmail.com>
2025-08-29 00:52:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const toggle = document.querySelector(".menu-toggle");
       menu.classList.toggle("show");
       toggle.textContent = menu.classList.contains("show") ? "✖" : "☰";
     });
+
+
+    function sendWhatsAppMessage(event, phone, img, name, price) {
+    event.preventDefault();
+    const message = `Hello, I am interested in this product:%0A
+*${name}*%0A
+Price: ${price}%0A
+Image: ${window.location.origin}/${img}`;
+    
+    const url = `https://wa.me/${phone}?text=${message}`;
+    window.open(url, '_blank');
+  }
